e313cb405d4b064478c55df7e0cf34db0b2b085c
maseeh1 <maseeh.shb@gmail.com>
2025-05-13 20:19:04 +0330

    create a homepage for navigating the tasks

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="min-h-screen flex flex-col items-center justify-center bg-white text-black p-4"
+  >
+    <h1 class="text-2xl font-semibold mb-6">Interview Tasks</h1>
+    <nav class="space-y-4 flex flex-col">
+      <NuxtLink to="/series/kaaenat" class="text-blue-600 hover:underline"
+        >Task 1 - Player Page</NuxtLink
+      >
+      <NuxtLink to="/video-player" class="text-blue-600 hover:underline"
+        >Task 2</NuxtLink
+      >
+      <NuxtLink to="/rating" class="text-blue-600 hover:underline"
+        >Task 3</NuxtLink
+      >
+    </nav>
+  </div>
+</template>
