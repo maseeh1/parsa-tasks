23c9f1600c717f6f58daf92795460d1fda15e2b0
maseeh1 <maseeh.shb@gmail.com>
2025-05-13 20:18:47 +0330

    add a page for viewing the custom player

---
+++ pages/video-player/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-screen my-10 flex flex-col gap-4 justify-center items-center">
+    <h1 class="text-3xl font-bold">Custom Video Player</h1>
+    <VideoPlayer />
+  </div>
+</template>
+
+<script setup>
+import VideoPlayer from "~/components/VideoPlayer.vue";
+</script>
